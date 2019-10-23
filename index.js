67172145847285d689816f38f5f7dbc19a25c7d2
alex.paclin <alex.paclin@gmail.com>
2019-10-23 16:06:01 +0500

    generator2

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-function* range(start, end) {
-    let current = start;
-    while (current <= end) {
-        yield current++;
-    }
+function *generator() {
+    yield;
+    yield;
 }
 
-function range2(start, end) {
-    let current = start;    
-    return {
-        [Symbol.iterator]() {
-            return {
-                next() {
-                    let result = { value: undefined, done: true };
-                    if (current <= end) {
-                        result.value = current++;
-                        result.done = false;
-                    }
-                    return result;
-                }
-            }
-        }
-    }
-}
-
-console.log('range');
-for (const num of range(1, 10)) {
-    console.log(num);
-}
-
-console.log('range2');
-for (const num of range2(1, 5)) {
-    console.log(num);
-}
+let iterator = generator();
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
