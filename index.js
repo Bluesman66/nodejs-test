b28a65357fe7968c1e552a78cce3e8750ed28671
alex.paclin <alex.paclin@gmail.com>
2020-05-19 14:13:47 +0500

    object enumeration

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-function testSpread(a, b, c) {    
-    console.log(a + b + c);
+function testSpread(a, b, c) {
+	console.log(a + b + c);
 }
 
 function testRest(...arr) {
-    let sum = 0;
+	let sum = 0;
 
-    arr.forEach(function(value) {
-        sum += value;
-    })
+	arr.forEach(function (value) {
+		sum += value;
+	});
 
-    console.log(sum);
+	console.log(sum);
 }
 
 function createCar(property, value) {
-    return {
-        [property]: value,
-        ['_' + property]: value,
-        [property.toUpperCase()]: value,
-        ['get' + property]() {
-            return this[property];
-        }
-    }
+	return {
+		[property]: value,
+		['_' + property]: value,
+		[property.toUpperCase()]: value,
+		['get' + property]() {
+			return this[property];
+		},
+	};
 }
 
-let arr = [1,2,3];
+let arr = [1, 2, 3];
 testSpread(...arr);
 
-testRest(1,2,3,4,5);
+testRest(1, 2, 3, 4, 5);
 
 let car = createCar('vin', 1);
 console.log(car);
 console.log(car.getvin());
+
+const ts = {
+	a: 1,
+	b: 2,
+	c: {
+		ca: 1,
+		cb: 2,
+		cc: 3,
+	},
+};
+
+for (let item in ts) {
+	console.log(`item: ${ts[item]}, type: ${typeof ts[item]}`);
+}
